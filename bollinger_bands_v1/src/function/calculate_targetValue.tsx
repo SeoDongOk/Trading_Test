96d4bb2edb54feb02706d88be18526fe7980f751
DongOk <hcan159@naver.com>
2023-09-06 23:03:34 +0900

    @add aixos
    get symbol of price at this time

---
+++ bollinger_bands_v1/src/function/calculate_targetValue.tsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+
+const rawData = async ()=>{
+    const params = {
+        headers:{
+            "content-type":"application/json",
+            // BINANCE_API_KEY is on .env
+            "apiKey":"BINANCE_API_KEY"
+        }
+    }
+    //
+    const result_api = await axios.get('https://fapi.binance.com/api/v3/ticker/price', params);
+    const data='';
+    return data
+}
+
+// Not using BollingerBand package
+const calculate_RawData = async () => {
+
+
+}
